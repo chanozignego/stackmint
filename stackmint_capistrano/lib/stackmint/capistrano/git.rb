4c8a68090c9bcc89783a03b41759b9579b73ea76
Maximiliano Felice <maximilianofelice@gmail.com>
2015-11-30 18:41:40 -0300

    Added port info in host registering

--- stackmint_capistrano/lib/stackmint/capistrano/git.rb
+++ stackmint_capistrano/lib/stackmint/capistrano/git.rb
@@ -7,14 +7,14 @@ configuration = Capistrano::Configuration.respond_to?(:instance) ?
 
 configuration.load do
 
-  def add_domain_to_known_hosts(hostname)
+  def add_domain_to_known_hosts(hostname, port=22)
     ip_address = "`dig +short A #{hostname}`"
     run_as_user user, "ssh-keygen -R #{hostname}"
     run_as_user user, "ssh-keygen -R #{ip_address}"
     run_as_user user, "ssh-keygen -R #{hostname},#{ip_address}"
-    run_as_user user, "ssh-keyscan -H #{hostname},#{ip_address} >> ~/.ssh/known_hosts"
-    run_as_user user, "ssh-keyscan -H #{ip_address} >> ~/.ssh/known_hosts"
-    run_as_user user, "ssh-keyscan -H #{hostname} >> ~/.ssh/known_hosts"
+    run_as_user user, "ssh-keyscan -p #{port} -H #{hostname},#{ip_address} >> ~/.ssh/known_hosts"
+    run_as_user user, "ssh-keyscan -p #{port} -H #{ip_address} >> ~/.ssh/known_hosts"
+    run_as_user user, "ssh-keyscan -p #{port} -H #{hostname} >> ~/.ssh/known_hosts"
   end
 
   namespace :git_config do
@@ -24,7 +24,7 @@ configuration.load do
       run_as_user user, "touch ~/.ssh/known_hosts"
       add_domain_to_known_hosts("github.com")
       add_domain_to_known_hosts("gitlab.redmintlabs.com")
-      add_domain_to_known_hosts("gitlab.demos.redmintlabs.com")
+      add_domain_to_known_hosts("gitlab.demos.redmintlabs.com", 1022)
     end
   end
 end
