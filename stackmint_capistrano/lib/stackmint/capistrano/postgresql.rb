dccf4bba118079bae619a9c83fdec1f2a676cf35
Martin Carniello <martin.carniello@dev.redmintlabs.com>
2016-02-03 14:43:39 -0300

    Test to get server hostname from capfile.

--- stackmint_capistrano/lib/stackmint/capistrano/postgresql.rb
+++ stackmint_capistrano/lib/stackmint/capistrano/postgresql.rb
@@ -144,6 +144,7 @@ configuration.load do
 
     desc "Create db dump and download, params: db_dump, server_dump."
     task :create_dump do |t, args|
+      puts "SERVER NAME: #{server.hostname}"
       
       db_dump = ENV['db_dump']
       server_dump = ENV['server_dump']
