0362f1c779c471fc4a4a4848e4859d8a1a8e05e9
Martin Carniello <martin.carniello@dev.redmintlabs.com>
2016-02-03 14:58:00 -0300

    Remove server parameter to create_dump task.

--- stackmint_capistrano/lib/stackmint/capistrano/postgresql.rb
+++ stackmint_capistrano/lib/stackmint/capistrano/postgresql.rb
@@ -142,12 +142,11 @@ configuration.load do
       set :backup, choice.empty? ? backups.last : choice
     end
 
-    desc "Create db dump and download, params: db_dump, server_dump."
+    desc "Create db dump and download, params: db_dump."
     task :create_dump do |t, args|
-      puts "SERVER NAME: #{capture("echo $CAPISTRANO:HOST$").strip}"
-      
+
       db_dump = ENV['db_dump']
-      server_dump = ENV['server_dump']
+      server_dump = capture("echo $CAPISTRANO:HOST$").strip
 
       if !db_dump.nil? && !server_dump.nil?
         date_format = Date.today.strftime("%d-%m-%Y")
