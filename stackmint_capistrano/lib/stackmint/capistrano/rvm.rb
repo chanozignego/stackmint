528ccefadf0dd405295707d84b063c7553b33ddb
Esteban Zignego <esteban.zignego@gmail.com>
2019-09-11 22:40:14 -0300

    change rvm version

--- stackmint_capistrano/lib/stackmint/capistrano/rvm.rb
+++ stackmint_capistrano/lib/stackmint/capistrano/rvm.rb
@@ -10,12 +10,13 @@ configuration.load do
 
     desc "Install the latest ruby patch with rvm"
     task :install, roles: :app do
-      run_as_user "root", "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
+      run_as_user "root", "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+"
       run_as_user "root", "curl -sSL https://get.rvm.io | bash -s stable"
     end
 
     task :install_ruby, roles: :app do
-      run_as_user "root", "rvm install --default 2.2.4"
+      run_as_user "root", "rvm install --default 2.5.1 --create"
     end
 
     task :add_user_to_rvm, roles: :app do
