db51d697f04ad8eddcb901dbcc288df01330f805
Esteban Zignego <esteban.zignego@gmail.com>
2019-11-04 08:43:52 -0300

    fix bug with old version of rvm

--- stackmint_capistrano/lib/stackmint/capistrano/rvm.rb
+++ stackmint_capistrano/lib/stackmint/capistrano/rvm.rb
@@ -10,7 +10,8 @@ configuration.load do
 
     desc "Install the latest ruby patch with rvm"
     task :install, roles: :app do
-      run_as_user "root", "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
+      # run_as_user "root", "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
+      run_as_user "root", "gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
       run_as_user "root", "curl -sSL https://get.rvm.io | bash -s stable"
     end
 
